fd5331bd4b989f81da5e11bf17d75b22df8f5494
phoenix <fy24h.huang@gmail.com>
2013-01-03 17:19:33 +0800

    update

---
+++ Src/Zing/Environment/ShellBuilder/ShellContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zing.Environment.ShellBuilder
+{
+    public class ShellContext
+    {
+        public ShellSettings Settings { get; set; }
+        public ShellDescriptor Descriptor { get; set; }
+        public ShellBlueprint Blueprint { get; set; }
+        public ILifetimeScope LifetimeScope { get; set; }
+        public IOrchardShell Shell { get; set; }
+    }
+}
