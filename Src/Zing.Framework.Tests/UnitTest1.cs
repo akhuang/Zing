e9a10cc69662628c7e3e06e38eacb9e34a2a5067
fy24h <fy24h.huang@gmail.com>
2013-08-14 22:45:56 +0800

    add test

---
+++ Src/Zing.Framework.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Zing.Framework.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void SumOfTwoNumbers()
+        {
+            Assert.AreEqual(5, 3 + 2);
+        }
+    }
+}
---
+++ Src/Zing/Data/SessionLocator.cs
@@ -0,0 +1,54 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Zing.Logging;
+
+namespace Zing.Data
+{
+    public class SessionLocator : ISessionLocator
+    {
+        private readonly ISessionFactoryHolder _sessionFactoryHolder;
+        private ISession _session;
+
+        public SessionLocator(ISessionFactoryHolder sessionFactoryHolder)
+        {
+            _sessionFactoryHolder = sessionFactoryHolder;
+            Logger = NullLogger.Instance;
+        }
+
+        public ILogger Logger { get; set; }
+
+        public ISession For(Type entityType)
+        {
+            Logger.Debug("Acquiring session for {0}", entityType);
+            Demand();
+
+            return _session;
+        }
+
+        public void Demand()
+        {
+            EnsureSession();
+
+            //if (_transaction == null)
+            //{
+            //    Logger.Debug("Creating transaction on Demand");
+            //    _transaction = _session.BeginTransaction(IsolationLevel.ReadCommitted);
+            //}
+        }
+
+        private void EnsureSession()
+        {
+            if (_session != null)
+            {
+                return;
+            }
+
+            var sessionFactory = _sessionFactoryHolder.GetSessionFactory();
+            Logger.Information("Opening database session");
+            _session = sessionFactory.OpenSession();
+        }
+    }
+}
