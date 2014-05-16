3fe001025cb64d167ecb9e1d5f369769d0967f50
phoenix <fy24h.huang@gmail.com>
2014-05-16 14:33:47 +0800

    update ui

---
+++ Src/Zing.Web/Scripts/app.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    $('.sidebar-left ul.sidebar-menu li a').click(function () {
+        "use strict";
+        $('.sidebar-left li').removeClass('active');
+        $(this).closest('li').addClass('active');
+        var checkElement = $(this).next();
+        if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+            $(this).closest('li').removeClass('active');
+            checkElement.slideUp('fast');
+        }
+        if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+            $('.sidebar-left ul.sidebar-menu ul:visible').slideUp('fast');
+            checkElement.slideDown('fast');
+        }
+        if ($(this).closest('li').find('ul').children().length == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    $(".btn-collapse-sidebar-left").click(function () {
+        "use strict";
+        $(".top-navbar").toggleClass("toggle");
+        $(".sidebar-left").toggleClass("toggle");
+        $(".page-content").toggleClass("toggle");
+        $(".icon-dinamic").toggleClass("rotate-180");
+    });
+});
