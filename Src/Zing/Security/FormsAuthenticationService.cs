774d3bcbf9e0b614d7474d138ab9cefec22326ed
phoenix <fy24h.huang@gmail.com>
2014-07-08 16:41:31 +0800

    add FormsAuthenticationService.cs

---
+++ Src/Zing/Security/FormsAuthenticationService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Zing.Framework.Security;
+using Zing.Environment.Configuration;
+using Zing.Mvc;
+using Zing.Logging;
+
+namespace Zing.Security
+{
+    public class FormsAuthenticationService : IAuthenticationService
+    {
+        private readonly ShellSettings _setting;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private IUser _signedInUser;
+        private bool _isAuthenticated;
+
+        public FormsAuthenticationService(ShellSettings settings, IHttpContextAccessor httpContextAccessor)
+        {
+            _setting = settings;
+            _httpContextAccessor = httpContextAccessor;
+            ExpirationTimeSpan = TimeSpan.FromDays(30);
+        }
+
+        public ILogger Logger { get; set; }
+
+        public TimeSpan ExpirationTimeSpan { get; set; }
+
+        #region IAuthenticationService Members
+
+        public void SignIn(IUser user, bool createPersistentCookie)
+        {
+            var now = DateTime.Now;
+            var userData = string.Concat(Convert.ToString(user.Id), ";", _setting.Name);
+
+        }
+
+        public void SignOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAuthenticatedUserForRequest(IUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUser GetAuthenticatedUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
--- Src/Zing/Security/IAuthenticationService.cs
+++ Src/Zing/Security/IAuthenticationService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Zing.Framework.Security
+namespace Zing.Security
 {
     public interface IAuthenticationService  
     {
