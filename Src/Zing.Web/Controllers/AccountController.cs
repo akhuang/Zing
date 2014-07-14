471351354b9e1e940fbf5d7365ce2f584d286da0
phoenix <fy24h.huang@gmail.com>
2014-07-14 15:00:11 +0800

    update logon

--- Src/Zing.Web/Controllers/AccountController.cs
+++ Src/Zing.Web/Controllers/AccountController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zing.Security;
 
 namespace Zing.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AccountController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
         //
         // GET: /Account/
 
@@ -17,8 +24,9 @@ namespace Zing.Web.Controllers
         }
 
         [HttpPost]
-        public ActionResult Logon(int id)
+        public ActionResult Logon(string userName, string userPwd)
         {
+            _authenticationService.SignIn(
             return View();
         }
     }
